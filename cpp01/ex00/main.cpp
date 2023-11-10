5a34e9e861e119eaf382d79715a5ebffdc5665ab
Khawla Ben hammou <kben-ham@e2r4p13.1337.ma>
2023-11-10 21:55:11 +0100

    cpp01 ex00 not finished yet

--- cpp01/ex00/main.cpp
+++ cpp01/ex00/main.cpp
@@ -3,8 +3,13 @@
 
 #include "Zombie.hpp"
 
-int main()
+int main(int ac, char **av)
 {
+	(void)av;
+	if (ac > 1) {
+		std::cout << "Error: invalid arguments" << std::endl;
+		return (1);
+	}
 	Zombie h_1;
 	Zombie *h_2;
 
