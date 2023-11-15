2e85b5d578e13cd31eebb6580b7b7dc39d118398
Khawla Ben hammou <kben-ham@e2r7p12.1337.ma>
2023-11-15 11:54:12 +0100

    update some errors

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: kben-ham <kben-ham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 22:18:01 by kben-ham          #+#    #+#             */
-/*   Updated: 2023/11/05 15:44:42 by kben-ham         ###   ########.fr       */
+/*   Updated: 2023/11/15 11:52:14 by kben-ham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ int main(int ac, char **av)
 	int	i;
 	int j;
 	char s;
+	std::string str;
 
 	j = 1;
 	if (ac == 1)
@@ -24,15 +25,16 @@ int main(int ac, char **av)
 	while (j < ac)
 	{
 		i = -1;
-		while (av[j][++i])
+		str = av[j];
+		while (str[++i])
 		{
-			if (std::islower(av[j][i]))
+			if (std::islower(str[i]))
 			{
-				s = std::toupper(av[j][i]);
+				s = std::toupper(str[i]);
 				std::cout << s;
 			}
 			else
-				std::cout << av[j][i];
+				std::cout << str[i];
 		}
 		j++;
 	}
