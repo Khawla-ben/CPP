32bc9759336a2ae6b3487428830c92ac80a5d819
Khawla Ben hammou <kben-ham@e2r7p14.1337.ma>
2023-11-21 17:40:31 +0100

    cpp02/ex01 not completed

---
+++ cpp02/ex01/Fixed.cpp
@@ -0,0 +1,60 @@
+
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+	f_p_Number = 0;
+	std::cout << "Default constructor called " << std::endl;
+}
+
+Fixed::Fixed(const int num)
+{
+	std::cout << "Int constructor called" << std::endl;
+	f_p_Number = num << number_frac;
+}
+Fixed::Fixed(const float num)
+{
+	std::cout << "Float constructor called" << std::endl;
+	f_p_Number = (int)num * 2^number_frac;//??//
+	f_p_Number = roundf(f_p_Number);
+}
+
+Fixed::Fixed(const Fixed &val)
+{
+	std::cout << "Copy constructor called " << std::endl;
+	*this = val;
+}
+
+Fixed &Fixed::operator=(const Fixed &a) {
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &a) { //self-assignment 
+		this->f_p_Number = a.f_p_Number;
+	}
+	return *this;
+}
+
+int Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return(this->f_p_Number);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	this->f_p_Number = raw;
+}
+
+float Fixed::toFloat(void) const
+{
+	
+}
+
+int Fixed::toInt(void) const
+{
+
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called " << std::endl;
+}
---
+++ cpp02/ex01/Fixed.hpp
@@ -0,0 +1,37 @@
+
+#pragma once
+
+#include <iostream>
+
+
+class  Fixed
+{
+private:
+
+	int f_p_Number;
+	static const int number_frac = 8;
+
+public:
+
+	Fixed();
+	Fixed(const int num);
+	Fixed(const float NUM);
+
+	Fixed(const Fixed &val);
+	Fixed &operator=(const Fixed &a);//Copy assignment operator called
+
+
+	// Fixed &operator=(const Fixed &a);//Copy assignment operator called
+	// • An overload of the insertion («) operator that inserts a floating-point representation
+	// of the fixed-point number into the output stream object passed as parameter.
+
+	void setRawBits(int const raw );//that sets the raw value of the fixed-point number.
+	int getRawBits(void) const;//that returns the raw value of the fixed-point value.
+
+
+	float toFloat(void) const; // fixed-point to floating-point.
+	int toInt(void) const; //fixed-point to an integer .
+
+	~Fixed();
+};
+
