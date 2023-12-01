681efabda8a02ffb4858e30783bb44d8442f58b8
Khawla Ben hammou <kben-ham@e2r7p14.1337.ma>
2023-12-01 20:48:23 +0100

    cpp 04/ex00

--- cpp04/ex00/main.cpp
+++ cpp04/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kben-ham <kben-ham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 16:17:52 by kben-ham          #+#    #+#             */
-/*   Updated: 2023/12/01 20:32:23 by kben-ham         ###   ########.fr       */
+/*   Updated: 2023/12/01 20:47:30 by kben-ham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,15 @@ int main()
 	const Animal* j = new Dog();
 	const Animal* i = new Cat();
 	
-	// std::cout << j->getType() << " " << std::endl;
-	// std::cout << i->getType() << " " << std::endl;
-	// i->makeSound();
-	// j->makeSound();
-	// meta->makeSound();
+	std::cout << j->getType() << " " << std::endl;
+	std::cout << i->getType() << " " << std::endl;
+	i->makeSound();
+	j->makeSound();
+	meta->makeSound();
 	
-	// delete (meta);
-	// delete (i);
-	// delete (j);
+	delete (meta);
+	delete (i);
+	delete (j);
 	
 	return 0;
 }
