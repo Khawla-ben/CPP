4395c4765bc7fc6452276830e21ba062d56948fa
KHAWLA BEN HAMMOU <115346619+Khawla-ben@users.noreply.github.com>
2024-02-16 14:46:39 +0100

    updates cpp07

--- cpp07/ex01/Iter.hpp
+++ cpp07/ex01/Iter.hpp
@@ -6,7 +6,7 @@
 /*   By: kben-ham <kben-ham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 06:36:15 by kben-ham          #+#    #+#             */
-/*   Updated: 2024/02/15 17:54:46 by kben-ham         ###   ########.fr       */
+/*   Updated: 2024/02/16 14:36:45 by kben-ham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,5 @@ template <typename T, typename U, typename S>
 void iter(T &array, int len_array, S (*function)(U&))
 {
     for(int i = 0; i < len_array; i++)
-    {
         function(array[i]);
-    }
 }
