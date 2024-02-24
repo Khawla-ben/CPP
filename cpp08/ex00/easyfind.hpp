3e58389b6ed1c17a775372e24d531e8296c88dc8
KHAWLA BEN HAMMOU <115346619+Khawla-ben@users.noreply.github.com>
2024-02-24 04:30:35 +0100

    updates cpp08

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: kben-ham <kben-ham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/02 13:46:38 by kben-ham          #+#    #+#             */
-/*   Updated: 2024/02/02 14:49:11 by kben-ham         ###   ########.fr       */
+/*   Updated: 2024/02/24 00:30:37 by kben-ham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,5 @@ void easyfind(T containner, int to_find)
     if (it != containner.end())
         std::cout << "I found it !!\n";
     else
-        std::cout << "OOPS !! there is not a '" << to_find << "' in this containner ." << std::endl;
+        throw std::out_of_range("OOPS !! I didn't found it");
 }
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -6,7 +6,26 @@
 /*   By: kben-ham <kben-ham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/02 14:35:03 by kben-ham          #+#    #+#             */
-/*   Updated: 2024/02/02 15:07:10 by kben-ham         ###   ########.fr       */
+/*   Updated: 2024/02/24 00:30:21 by kben-ham         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "easyfind.hpp"
+#include <vector>
+#include <deque>
+#include <forward_list>
+#include <list>
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kben-ham <kben-ham@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/02 14:35:03 by kben-ham          #+#    #+#             */
+/*   Updated: 2024/02/24 00:30:04 by kben-ham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,41 +39,55 @@
 
 int main()
 {
-    std::vector<int> vec;
-    vec.push_back(1);
-    vec.push_back(2);
-    vec.push_back(3);
     int to_find = 3;
-    easyfind(vec, to_find);
-    
-    std::cout << "******************" << std::endl;
-    
-    std::deque<int> deq;
-    deq.push_back(10);
-    deq.push_front(20);
-    deq.push_back(30);
-    deq.push_front(15);
-    easyfind(deq, to_find);
-    
-    std::cout << "******************" << std::endl;
-    
-    int array_to_initialise[] = {1, 2, 3};
-    std::forward_list<int> f_list;
-    for (size_t i = 0; i < sizeof(array_to_initialise) / sizeof(array_to_initialise[0]); ++i) {
-        f_list.push_front(array_to_initialise[i]);
+    try {
+        std::vector<int> vec;
+        vec.push_back(1);
+        vec.push_back(2);
+        vec.push_back(3);
+        easyfind(vec, to_find);
     }
-    easyfind(f_list, to_find);
-    
-    std::cout << "******************" << std::endl;
-    
-    std::list<int> d_list;
-    for (int i = 0; i < 5; ++i) {
-        d_list.push_back(i * 2);
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
     }
-    for (std::list<int>::iterator it = d_list.begin(); it != d_list.end(); ++it) 
-        std::cout << *it << " ";
-    std::cout << std::endl;
-    easyfind(d_list, to_find);
-    
-
+        std::cout << "******************" << std::endl;
+    try {
+        std::deque<int> deq;
+        deq.push_back(10);
+        deq.push_front(20);
+        deq.push_back(30);
+        deq.push_front(15);
+        easyfind(deq, to_find);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+        
+        std::cout << "******************" << std::endl;
+    try {
+        int array_to_initialise[] = {1, 2, 3};
+        std::forward_list<int> f_list;
+        for (size_t i = 0; i < sizeof(array_to_initialise) / sizeof(array_to_initialise[0]); ++i) {
+            f_list.push_front(array_to_initialise[i]);
+        }
+        easyfind(f_list, to_find);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+        std::cout << "******************" << std::endl;
+    try {
+        std::list<int> d_list;
+        for (int i = 0; i < 5; ++i) {
+            d_list.push_back(i * 2);
+        }
+        easyfind(d_list, to_find);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }  
 }
